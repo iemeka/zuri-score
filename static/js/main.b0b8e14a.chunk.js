7fc02ede15cb375bcaf697529df0315787015340
iemeka <mekstines@gmail.com>
2021-05-21 16:56:10 +0100

    Updates

--- static/js/main.b0b8e14a.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpzuri-score"]=this["webpackJsonpzuri-score"]||[]).push([[0],{11:function(e,n,c){"use strict";c.r(n);var t=c(1),r=c.n(t),i=c(3),s=c.n(i),o=(c(8),c(9),c(0));var u=function(){return Object(o.jsx)("div",{className:"container",children:Object(o.jsx)("h1",{children:" HEY NOW BROWN COW"})})};s.a.render(Object(o.jsx)(r.a.StrictMode,{children:Object(o.jsx)(u,{})}),document.getElementById("root"))},8:function(e,n,c){},9:function(e,n,c){}},[[11,1,2]]]);
-//# sourceMappingURL=main.b0b8e14a.chunk.js.map
